6493be2691a48fa8dc0b853d83cffd1d231172c7
lucaxgomex <lg.projectdev@gmail.com>
2025-12-26 11:01:17 -0300

    dev: pipe operator on build function

--- lib/reposts_generator.ex
+++ lib/reposts_generator.ex
@@ -1,23 +1,17 @@
 defmodule RepostsGenerator do
-  @moduledoc """
-  Documentation for `RepostsGenerator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> RepostsGenerator.hello()
-      :world
-
-  """
-
   def build(filename) do
+    """
     case File.read("reports/#{filename}") do
       {:ok, result} -> result
       {:error, reason} -> reason
       _ -> "Any case."
     end
+    """
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file()
   end
+
+  defp handle_file({:ok, file_content}), do: file_content
+  defp handle_file({:error, reason}), do: "Error while opening file: #{reason}"
 end
