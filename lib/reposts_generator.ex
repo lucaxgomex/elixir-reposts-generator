f4d9a71d6d0ed289003f33e5621ff560fe19c021
lucaxgomex <lg.projectdev@gmail.com>
2026-01-20 14:53:35 -0300

    dev: creating a parallel execution, plus test to the code

--- lib/reposts_generator.ex
+++ lib/reposts_generator.ex
@@ -20,10 +20,17 @@ defmodule RepostsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of strings"}
+  end
+
   def build_from_many(filenames) do
-    filenames
-    |> Task.async_stream(&build/1)
-    |> Enum.reduce(report_acc(), fn {:ok, result}, report)
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
   end
 
   def empty_map?(map) when is_map(map) and map_size(map) == 0, do: true
@@ -34,7 +41,7 @@ defmodule RepostsGenerator do
 
   def fetch_higher_cost(_report,_option), do: {:error, "Invalid option"}
 
-  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
@@ -42,13 +49,26 @@ defmodule RepostsGenerator do
     # |> Map.put("users", users)
     # |> Map.put("foods", foods)
 
-    %{report | "users" => users, "foods" => foods}
+    build_report(foods, users)
+  end
+
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{"foods" => foods2, "users" => users2}) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
   end
 
   defp report_acc do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
 end
